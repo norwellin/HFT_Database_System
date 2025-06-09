990f4e8526cd614022c6c45ced53c73869766199
norwellin <norwellin@gmail.com>
2025-06-09 17:24:20 +0200

    branch merge

--- src/databaseTest/Test.java
+++ src/databaseTest/Test.java
@@ -5,11 +5,22 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.SpringLayout;
+import net.miginfocom.swing.MigLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 
 public class Test extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTable table;
 
 	/**
 	 * Launch the application.
@@ -32,11 +43,34 @@ public class Test extends JFrame {
 	 */
 	public Test() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 836, 566);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(new MigLayout("", "[][][][grow][][][][][][][][][][][][][][][]", "[][grow][][grow][][][][][grow][][][][][]"));
+		
+		JButton btnNewButton = new JButton("New button");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		
+		JLabel lblNewLabel = new JLabel("Test");
+		lblNewLabel.setForeground(new Color(0, 0, 64));
+		lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 28));
+		contentPane.add(lblNewLabel, "cell 10 0");
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, "cell 3 1 15 1,grow");
+		
+		JScrollPane scrollPane = new JScrollPane();
+		contentPane.add(scrollPane, "cell 3 3,grow");
+		
+		table = new JTable();
+		contentPane.add(table, "cell 3 8,grow");
+		contentPane.add(btnNewButton, "cell 18 13");
 	}
 
 }
